22329d730a222fac4a7b47019d7885920e31b87d
Sandra Gufler <sandragufler@MBP-von-Sandra.yallo.box>
2022-11-16 10:29:22 +0100

    added footer with portfolio and github link

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import { AiFillGithub, AiFillHeart } from 'react-icons/ai';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer-container'>
+      <p>2022 Wanty, cool that you are here</p>
+      <p className='icons'>
+        <a href='https://sandragufler.se'>
+          <AiFillHeart />
+        </a>
+        <a href='https://www.linkedin.com/in/sandragufler/'>
+          <AiFillGithub />
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
