50644f20872145d73551b5f2485f773d939d19db
Sandra <sandra.gufler@gmx.ch>
2022-11-17 16:23:13 +0100

    trying another env variable

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
+const stripe = new Stripe(process.env.STRIPE_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
